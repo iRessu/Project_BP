f82550e3a64f591027e7b5b3144166dfafb61762
iRessu <109947890+iRessu@users.noreply.github.com>
2024-05-06 14:43:42 +0200

    Beginning with SoundManager

---
+++ Project_BP/Assets/Game/Nicklas/Scripts/UI/AudioManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
